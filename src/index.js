50b861fd5d73b7708e774dab7c805134245e5cec
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-09-16 00:18:53 +0300

    some_comments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  var length = bracketsConfig.length;
-  var arr = [];
+  var length = bracketsConfig.length; //количсетво пар скобок
+  var arr = []; //хранит количество открытых/закрытых скобок каждого типа
   for (var p = 0;p < length;p++)
   {
       arr.push(0);
-  };
-  for (var i = 0; i < str.length; i++)
+  }; //придаём всем значением ноль
+  for (var i = 0; i < str.length; i++) //считываем посимвольно строку
   {
-      for (var j = 0; j < length; j++)
+      for (var j = 0; j < length; j++) //перебираем возможные скобки
       {
-          if (str[i] == bracketsConfig[j][0])
+          if (str[i] == bracketsConfig[j][0]) //проверка на наличие открывающих скобок
           {
-              arr[j]++;
+              arr[j]++; //инкремент значения скобок типа j
           };
-          if (str[i] == bracketsConfig[j][1])
+          if (str[i] == bracketsConfig[j][1]) //проверка на наличие закрывающих скобок
           {
-              arr[j]--;
+              arr[j]--; //денкремент значения скобок типа j
           };
-          if (arr[j] < 0)
+          if (arr[j] < 0) //проверяем не имеем ли больше закрывающих скобок чем открывающих
           {
               return false;
           };
       }
   }
-  for (var d = 0; d < length; d++)
+  for (var d = 0; d < length; d++) //проверяем у скобок типа d равенство открывающихся и закрывающихся скобок
   {
       if (arr[d] != 0)
       {
