f5089b301c12843ddc9e5d5baa4e00346b90c8e7
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-09-16 02:47:12 +0300

    Finished

--- src/index.js
+++ src/index.js
@@ -1,56 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  var length = bracketsConfig.length; //количсетво пар скобок
-  var arr = []; //хранит количество открытых/закрытых скобок каждого типа
-  for (var p = 0;p < length;p++)
+  var size = bracketsConfig.length;
+  var repeats = str.length/2;
+  for (var i = 0; i < repeats; i++)
   {
-      arr.push(0);
-  }; //придаём всем значением ноль
-  var check = new Array();
-   /*на каждый вид скобок i -> при открытии скобки создаётся массив i-j -> хранящий значение arr каждого вида скобок i-j-k; при закрытии скобки i сверяется последний элемент массива i-j с тем статусом который имеется сейчас и если они совпадают удаляется последний элемент мссива i-j если же нет
-  возвращается значение false
-  */
-  for (var i = 0; i < length; i++) // i = индекс вида скобок
-  {
-      check[i] = new Array();
-      check[i] = 0;
-  };
-  var temp = [];//запишем массив хранящий статус всех скобок
-  for (var s = 0; s < str.length; s++) //считываем посимвольно строку
-  {
-      for (var i = 0; i < length; i++) //перебираем возможные скобки
+      for (var j = 0; j < str.length-1;j++)
       {
-          if (str[s] == bracketsConfig[i][0]) //проверка на наличие открывающих скобок
+          for (var k = 0; k < size; k++)
           {
-              for (var z = 0; z < length; z++)
+              if ((str[j] == bracketsConfig[k][0]) && (str[j+1] == bracketsConfig[k][1]) )
               {
-                  temp[z] = arr[z]; // хранит статус скобки z
+                  var temp = [];
+                  for (var p = 0; p < str.length; p++)
+                  {
+                      if((p == j) || (p ==j+1)) continue;
+                      temp.push(str[p]);
+                  };
+                  str = temp;
               }
-              check[i].push(temp);//i - скобка открывающая j-очередное открытие k-статус скобки
-              arr[i]++; //инкремент значения скобок типа j
-          };
-          if (str[s] == bracketsConfig[i][1]) //проверка на наличие закрывающих скобок
-          {
-              arr[i]--; //денкремент значения скобок типа j
-              for (var z = 0; z < length; z++)
-              {
-                  if (check[i][check[i].length][z] != arr[z])
-                  return false;
-              }
-              check[i].pop();
-          };
-          if (arr[i] < 0) //проверяем не имеем ли больше закрывающих скобок чем открывающих
-          {
-              return false;
-          };
+          }
       }
   }
-  for (var d = 0; d < length; d++) //проверяем у скобок типа d равенство открывающихся и закрывающихся скобок
+
+  if (str.length == 0)
   {
-      if (arr[d] != 0)
-      {
-          return false;
-      }
-  };
-  return true;
+      return true;
+  }
+  else
+  return false;
 }
