3cdfc2c5a4188b586ec1959f1285bbb991c10e72
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-10-04 20:42:14 +0300

    edited

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
+module.exports = function check(str, bracketsConfig) {
 // your solution
-let size = bracketsConfig.length;
-for (let j = 0; j < str.length - 1; j++) {
-    for (let k = 0; k < size; k++) {
-        if ((str[j] == bracketsConfig[k][0]) && (str[j + 1] == bracketsConfig[k][1])) {
-            str = str.slice(0, j) + str.slice(j + 2, str.length);
-            j = 0;
-        }
-    }
-}
+// your solution
+  let size = bracketsConfig.length;
+  let repeats = str.length/2;
+  for (let i = 0; i < repeats; i++)
+  {
+      for (let j = 0; j < str.length-1;j++)
+      {
+          for (let k = 0; k < size; k++)
+          {
+              if ((str[j] == bracketsConfig[k][0]) && (str[j+1] == bracketsConfig[k][1]) )
+              {
+                  let temp = [];
+                  for (let p = 0; p < str.length; p++)
+                  {
+                      if((p == j) || (p ==j+1)) continue;
+                      temp.push(str[p]);
+                  };
+                  str = temp;
+              }
+          }
+      }
+  }
 
-if (str.length == 0) {
-    return true;
-} else
-    return false;
+  if (str.length == 0)
+  {
+      return true;
+  }
+  else
+  return false;
 }
