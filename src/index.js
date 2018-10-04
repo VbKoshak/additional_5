1305b2831b2744cc1a29652e53aa1bbaf558d206
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-10-04 20:18:57 +0300

    edited

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-  var size = bracketsConfig.length;
-  var repeats = str.length/2;
-  for (var i = 0; i < repeats; i++)
-  {
-      for (var j = 0; j < str.length-1;j++)
-      {
-          for (var k = 0; k < size; k++)
-          {
-              if ((str[j] == bracketsConfig[k][0]) && (str[j+1] == bracketsConfig[k][1]) )
-              {
-                  var temp = [];
-                  for (var p = 0; p < str.length; p++)
-                  {
-                      if((p == j) || (p ==j+1)) continue;
-                      temp.push(str[p]);
-                  };
-                  str = temp;
-              }
-          }
-      }
-  }
+// your solution
+let size = bracketsConfig.length;
+for (let j = 0; j < str.length - 1; j++) {
+    for (let k = 0; k < size; k++) {
+        if ((str[j] == bracketsConfig[k][0]) && (str[j + 1] == bracketsConfig[k][1])) {
+            str = str.slice(0, j) + str.slice(j + 2, str.length);
+            j = 0;
+        }
+    }
+}
 
-  if (str.length == 0)
-  {
-      return true;
-  }
-  else
-  return false;
+if (str.length == 0) {
+    return true;
+} else
+    return false;
 }
